b6e98418c12d6d6a504a124b29523a4b57298ed4
our-s <63934022+our-s@users.noreply.github.com>
2020-12-02 21:07:12 -0300

    Add files

---
+++ PilhaCorreta.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class PilhaCorreta {
+	
+	protected int capacity;
+	protected final int MAX = 8;
+	protected Integer [] pilha;
+	protected int size;
+	
+	PilhaCorreta() {
+		capacity = MAX;
+		pilha = new Integer[capacity];
+		size = 0;
+	}
+	
+	public static BufferedReader leitor(String path) {
+		try {
+			FileInputStream fstream = new FileInputStream(path);
+			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+			return br;
+		} catch (Exception e) {
+			System.out.println("Erro ao ler arquivo " + path + " : " + e.getMessage());
+			return null;
+		}
+	}
+	
+	public static BufferedWriter escritor(String path) {
+		try {
+			FileOutputStream fstream = new FileOutputStream(path);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fstream));
+			return bw;
+		} catch (Exception e) {
+			System.out.println("Erro ao escrever no arquivo " + path + " : " + e.getMessage());
+			return null;
+		}
+	}
+	
+	public void add(int newElement) {
+		if(size == capacity){
+		capacity += MAX*2;
+		Integer[ ] tmp = new Integer[capacity] ;
+		for(int j = 0 ;j<size ; j++){
+		tmp [ j ] = pilha [ j ] ;
+		pilha = tmp ;
+									}	
+		}
+	pilha [size] = newElement ;
+	size++;	
+	}
+	
+	// public int remove() {
+	// 	int tmp = pilha[size];
+	// 	size = size - 1;
+	// 	return tmp ;
+	// }
+	
+	public void limparPilha() {
+		int i;
+		for (i=0; i < pilha.length; i++) pilha[i] = null;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			String dirArqLog = "C:\\Users\\guine\\OneDrive\\Área de Trabalho\\EP3\\log.txt";			// Insira aqui o caminho do arquivo que recebera o log
+			String dirArqTempo = "C:\\Users\\guine\\OneDrive\\Área de Trabalho\\EP3\\tempo.csv";		// Insira aqui o caminho do arquivo que recebera o tempo de processamento
+			String dirPastaEntradas = "C:\\Users\\guine\\OneDrive\\Área de Trabalho\\EP3\\Entradas";	// Insira aqui o caminho da pasta que contem os arquivos de entrada
+
+//            String dirArqLog = "/Users/stacks-efficiency/saidas/log_PilhaIngenua.txt";
+//            String dirArqTempo = "/Users/stacks-efficiency/saidas/tempo_PilhaIngenua.csv";
+//            String dirPastaEntradas = "/Users/stacks-efficiency/entradas";
+
+			BufferedWriter log = escritor(dirArqLog);
+			BufferedWriter tempo = escritor(dirArqTempo);
+			tempo.write("Nome do Arquivo;Tempo de Processamento(ms);Quantidade de Linhas"); // Cabecalho do arquivo dos tempos de processamento
+			tempo.newLine();
+			
+			File directoryPath = new File(dirPastaEntradas);
+			String contents[] = directoryPath.list(); // Lista com todos os arquivos existentes na pasta de entrada
+			
+			PilhaCorreta pilhaCorreta = new PilhaCorreta();
+			BufferedReader leitor;
+			String str;
+			
+			for (int i = 0; i < contents.length; i++) { // Percorre os arquivos da pasta de entrada
+				System.out.println("Processando Arquivo: " + contents[i]);
+				log.write("Processando Arquivo: " + contents[i]);
+				log.newLine();
+				log.newLine();
+				
+				leitor = leitor(dirPastaEntradas + "\\" + contents[i]);
+//                leitor = leitor(dirPastaEntradas + "/" + contents[i]);
+
+				str = leitor.readLine();
+				int qntLinhas = 0;
+				long t = System.currentTimeMillis();
+				
+				while (str != null) { // Percorre as linhas do arquivo
+					qntLinhas++;
+					
+					if (str.trim().equals("")) {
+					//log.write(String.valueOf(pilhaCorreta.remove()));	
+					log.newLine();
+					} else {
+						pilhaCorreta.add(Integer.parseInt(str));
+					}
+					str = leitor.readLine();
+				}
+				t = System.currentTimeMillis() - t;
+				
+				tempo.write(contents[i] + ";" + t + ";" + qntLinhas);
+				tempo.newLine();
+				if (i != contents.length - 1) log.newLine();
+				pilhaCorreta.limparPilha();
+				leitor.close();
+			}
+			log.close();
+			tempo.close();
+			
+		} catch (Exception e) {
+			System.out.println("Erro ao executar o programa (Possivelmente Arquivo de Entrada Corrompido): " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
